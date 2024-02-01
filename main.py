a8bf2234a3ec35efaf98db8d0b0d1719f89ce216
Carlos Bonilla <carlosbonilla@isboli.com>
2024-02-01 09:22:22 -0600

    count how many words are in the book

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 def main():
 
+  number_of_words = 0
+
   with open("books/frankenstein.txt", "r") as f:
       file_contents = f.read()
   
-  print(file_contents)
+  number_of_words = count_words(file_contents)
 
+  print( number_of_words )
+
+
+def count_words(text):
+  words = text.split()
+
+  return len(words)
+   
 
 main()
