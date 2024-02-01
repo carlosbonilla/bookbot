9f5d78916128d14eb64b11ced4a9ffd62850dc68
Carlos Bonilla <carlosbonilla@isboli.com>
2024-02-01 09:46:38 -0600

    count every letter

--- main.py
+++ main.py
@@ -3,17 +3,32 @@ def main():
   number_of_words = 0
 
   with open("books/frankenstein.txt", "r") as f:
-      file_contents = f.read()
+      book = f.read()
   
-  number_of_words = count_words(file_contents)
+  number_of_words = count_words(book)
+  number_of_letters = count_letters(book)
 
-  print( number_of_words )
+  print(number_of_words)
+  print(number_of_letters)
 
 
-def count_words(text):
-  words = text.split()
+
+def count_words(book):
+  words = book.split()
 
   return len(words)
+
+def count_letters(book):
+   letters = {}
+
+   for each_letter in (book):
+      key = each_letter.lower()
+      if key in letters:
+        letters[key] += 1
+      else:
+        letters[key] = 1
+
+   return letters
    
 
 main()
